3247bbafaf12a394637a31665a97904d5a16848d
William Simas <94724763+williamsimass@users.noreply.github.com>
2025-08-17 12:43:42 -0300

    Vite att
    ss

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    include: ["xlsx"], // força Vite a otimizar o pacote xlsx
+  },
+  build: {
+    rollupOptions: {
+      external: [], // garante que não vai excluir o xlsx do bundle
     },
   },
 })
